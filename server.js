de660c51584bfd62b7ffc43f71118a75da08840d
luanaCristina <luanacrystinne@msn.com>
2024-05-07 19:11:12 -0300

    adicionando o código que fizemos na sala de aula

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const app = express()
+const { MongoClient } = require("mongodb-legacy")
+const url = "mongodb+srv://projetodb:projetodb@cluster0.7b2pbg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const ObjectId = require("mongodb-legacy").ObjectId
 
-//mongodb+srv://projetodb:<password>@cluster0.7b2pbg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const client = new MongoClient(url)
+//cria o banco de dados com o nome dPessoas (create database dPessoais)
+const db = client.db("d-pessoais");
+//cria uma collection com o nome crud (create table crud)
+const collection = db.collection("crud")
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}))
@@ -10,7 +18,9 @@ app.get('/index', (req, res)=>{
     res.render('index.ejs')
 })
 
-app.listen(3001, function(){
+
+
+app.listen(3003, function(){
     console.log("o nosso servidor está rodando na porta 3000")
 })
 
@@ -18,7 +28,28 @@ app.get("/ver", (req, res) => {
     res.send("Olá mundo!")
 })
 
-app.post('/show', (req, res) => {
-    console.log(req.body)
+app.post("/show", (req, res) => {
+    collection.insertOne(req.body, (err, result)=> {
+        if(err) return console.log(err)
+        console.log("SALVOU COM SUCESSO NO NOSSO BANCO DE DADOS")
+        res.redirect("/show")
+        collection.find().toArray((err, results) =>{
+            console.log(results)
+        })
+       })
+    })
+
+app.get('/', (req, res) => {
+    console.log("cursor")
+    let cursor = db.collection('crud').find(ObjectId(id))
 })
+
+app.get('/show', (req, res) => {
+    collection.find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', {crud: results})
+    })
+})
+
+app.route('/edit/:id')
 
